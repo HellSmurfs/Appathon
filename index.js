4611ddb18f1f5811a167e46b6b42487bfe86ee7e
wlongxiang <wlongxiang1119@gmail.com>
2017-09-19 11:44:46 +0200

    update info about Amsterdam

--- index.js
+++ index.js
@@ -1,80 +1,86 @@
-
 // 1. Text strings =====================================================================================================
 //    Modify these strings and messages to change the behavior of your Lambda function
 
 const languageStrings = {
     'en': {
         'translation': {
-            'WELCOME' : "Welcome to Gloucester Guide!",
-            'HELP'    : "Say about, to hear more about the city, or say coffee, breakfast, lunch, or dinner, to hear local restaurant suggestions, or say recommend an attraction, or say, go outside. ",
-            'ABOUT'   : "Gloucester Massachusetts is a city on the Atlantic Ocean.  A popular summer beach destination, Gloucester has a rich history of fishing and ship building.",
-            'STOP'    : "Okay, see you next time!"
+            'WELCOME': "Welcome to Amsterdam",
+            'HELP': "Say about, to hear more about the city, or say coffee, breakfast, lunch, or dinner, to hear local restaurant suggestions, or say recommend an attraction, or say, go outside. ",
+            'ABOUT': "Amsterdam is the Netherlands’ capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city’s 17th-century Golden Age. Its Museum District houses the Van Gogh Museum, works by Rembrandt and Vermeer at the Rijksmuseum, and modern art at the Stedelijk. Cycling is key to the city’s character, and there are numerous bike paths.",
+            'STOP': "Okay, see you next time, my Lord!"
         }
     }
     // , 'de-DE': { 'translation' : { 'TITLE'   : "Local Helfer etc." } }
 };
 const data = {
-    "city"        : "Gloucester",
-    "state"       : "MA",
-    "postcode"    : "01930",
-    "restaurants" : [
-        { "name":"Zeke's Place",
-            "address":"66 East Main Street", "phone": "978-283-0474",
+    "city": "Amsterdam",
+    "state": "Noord Holland",
+    "postcode": "1101 BE",
+    "restaurants": [
+        {
+            "name": "Zeke's Place",
+            "address": "66 East Main Street",
+            "phone": "978-283-0474",
             "meals": "breakfast, lunch",
             "description": "A cozy and popular spot for breakfast.  Try the blueberry french toast!"
         },
-        { "name":"Morning Glory Coffee Shop",
-            "address":"25 Western Avenue", "phone": "978-281-1851",
+        {
+            "name": "Morning Glory Coffee Shop",
+            "address": "25 Western Avenue",
+            "phone": "978-281-1851",
             "meals": "coffee, breakfast, lunch",
             "description": "A homestyle diner located just across the street from the harbor sea wall."
         },
-        { "name":"Sugar Magnolias",
-            "address":"112 Main Street", "phone": "978-281-5310",
+        {
+            "name": "Sugar Magnolias",
+            "address": "112 Main Street",
+            "phone": "978-281-5310",
             "meals": "breakfast, lunch",
             "description": "A quaint eatery, popular for weekend brunch.  Try the carrot cake pancakes."
         },
-        { "name":"Seaport Grille",
-            "address":"6 Rowe Square", "phone": "978-282-9799",
+        {
+            "name": "Seaport Grille",
+            "address": "6 Rowe Square",
+            "phone": "978-282-9799",
             "meals": "lunch, dinner",
             "description": "Serving seafood, steak and casual fare.  Enjoy harbor views on the deck."
         },
-        { "name":"Latitude 43",
-            "address":"25 Rogers Street", "phone": "978-281-0223",
+        {
+            "name": "Latitude 43",
+            "address": "25 Rogers Street",
+            "phone": "978-281-0223",
             "meals": "lunch, dinner",
             "description": "Features artsy decor and sushi specials.  Live music evenings at the adjoining Minglewood Tavern."
         },
-        { "name":"George's Coffee Shop",
-            "address":"178 Washington Street", "phone": "978-281-1910",
+        {
+            "name": "George's Coffee Shop",
+            "address": "178 Washington Street",
+            "phone": "978-281-1910",
             "meals": "coffee, breakfast, lunch",
             "description": "A highly rated local diner with generously sized plates."
         },
 
     ],
-    "attractions":[
+    "attractions": [
         {
-            "name": "Whale Watching",
-            "description": "Gloucester has tour boats that depart twice daily from Rogers street at the harbor.  Try either the 7 Seas Whale Watch, or Captain Bill and Sons Whale Watch. ",
+            "name": "Rijksmuseum",
+            "description": "The Rijksmuseum is a Dutch national museum dedicated to arts and history in Amsterdam. The museum is located at the Museum Square in the borough Amsterdam South, close to the Van Gogh Museum, the Stedelijk Museum Amsterdam, and the Concertgebouw.",
             "distance": "0"
         },
         {
-            "name": "Good Harbor Beach",
-            "description": "Facing the Atlantic Ocean, Good Harbor Beach has huge expanses of soft white sand that attracts hundreds of visitors every day during the summer.",
+            "name": "Van Gogh Museum",
+            "description": "he Van Gogh Museum is an art museum dedicated to the works of Vincent van Gogh and his contemporaries in Amsterdam in the Netherlands.",
             "distance": "2"
         },
         {
-            "name": "Rockport",
-            "description": "A quaint New England town, Rockport is famous for rocky beaches, seaside parks, lobster fishing boats, and several art studios.",
+            "name": "Anne Frank House",
+            "description": "The Anne Frank House is a writer's house and biographical museum dedicated to Jewish wartime diarist Anne Frank. The building is located on a canal called the Prinsengracht, close to the Westerkerk, in central Amsterdam in the Netherlands.",
             "distance": "4"
-        },
-        {
-            "name": "Fenway Park",
-            "description": "Home of the Boston Red Sox, Fenway park hosts baseball games From April until October, and is open for tours. ",
-            "distance": "38"
         }
     ]
 }
 
-const SKILL_NAME = "Gloucester Guide";
+const SKILL_NAME = "Amsterdam Guide";
 
 // Weather courtesy of the Yahoo Weather API.
 // This free API recommends no more than 2000 calls per day
@@ -89,7 +95,7 @@ const myAPI = {
 
 const Alexa = require('alexa-sdk');
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
 
     // alexa.appId = 'amzn1.echo-sdk-ams.app.1234';
@@ -151,15 +157,15 @@ const handlers = {
         var restaurantName = this.attributes['restaurant'];
         var restaurantDetails = getRestaurantByName(restaurantName);
 
-        var say = restaurantDetails.name
-            + ' is located at ' + restaurantDetails.address
-            + ', the phone number is ' + restaurantDetails.phone
-            + ', and the description is, ' + restaurantDetails.description
-            + '  I have sent these details to the Alexa App on your phone.  Enjoy your meal! <say-as interpret-as="interjection">bon appetit</say-as>' ;
+        var say = restaurantDetails.name +
+            ' is located at ' + restaurantDetails.address +
+            ', the phone number is ' + restaurantDetails.phone +
+            ', and the description is, ' + restaurantDetails.description +
+            '  I have sent these details to the Alexa App on your phone.  Enjoy your meal! <say-as interpret-as="interjection">bon appetit</say-as>';
 
-        var card = restaurantDetails.name + '\n' + restaurantDetails.address + '\n'
-            + data.city + ', ' + data.state + ' ' + data.postcode
-            + '\nphone: ' + restaurantDetails.phone + '\n';
+        var card = restaurantDetails.name + '\n' + restaurantDetails.address + '\n' +
+            data.city + ', ' + data.state + ' ' + data.postcode +
+            '\nphone: ' + restaurantDetails.phone + '\n';
 
         this.response.cardRenderer(SKILL_NAME, card);
         this.response.speak(say);
@@ -175,10 +181,10 @@ const handlers = {
 
         var attraction = randomArrayElement(getAttractionsByDistance(distance));
 
-        var say = 'Try '
-            + attraction.name + ', which is '
-            + (attraction.distance == "0" ? 'right downtown. ' : attraction.distance + ' miles away. Have fun! ')
-            + attraction.description;
+        var say = 'Try ' +
+            attraction.name + ', which is ' +
+            (attraction.distance == "0" ? 'right downtown. ' : attraction.distance + ' miles away. Have fun! ') +
+            attraction.description;
 
         this.response.speak(say);
         this.emit(':responseReady');
@@ -186,7 +192,7 @@ const handlers = {
 
     'GoOutIntent': function () {
 
-        getWeather( ( localTime, currentTemp, currentCondition) => {
+        getWeather((localTime, currentTemp, currentCondition) => {
             // time format 10:34 PM
             // currentTemp 72
             // currentCondition, e.g.  Sunny, Breezy, Thunderstorms, Showers, Rain, Partly Cloudy, Mostly Cloudy, Mostly Sunny
@@ -194,10 +200,10 @@ const handlers = {
             // sample API URL for Irvine, CA
             // https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22irvine%2C%20ca%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 
-            var say = 'It is ' + localTime
-                + ' and the weather in ' + data.city
-                + ' is '
-                + currentTemp + ' and ' + currentCondition;
+            var say = 'It is ' + localTime +
+                ' and the weather in ' + data.city +
+                ' is ' +
+                currentTemp + ' and ' + currentCondition;
             this.response.speak(say);
             this.emit(':responseReady');
 
@@ -239,7 +245,7 @@ function getRestaurantsByMeal(mealtype) {
     var list = [];
     for (var i = 0; i < data.restaurants.length; i++) {
 
-        if(data.restaurants[i].meals.search(mealtype) >  -1) {
+        if (data.restaurants[i].meals.search(mealtype) > -1) {
             list.push(data.restaurants[i]);
         }
     }
@@ -251,7 +257,7 @@ function getRestaurantByName(restaurantName) {
     var restaurant = {};
     for (var i = 0; i < data.restaurants.length; i++) {
 
-        if(data.restaurants[i].name == restaurantName) {
+        if (data.restaurants[i].name == restaurantName) {
             restaurant = data.restaurants[i];
         }
     }
@@ -264,7 +270,7 @@ function getAttractionsByDistance(maxDistance) {
 
     for (var i = 0; i < data.attractions.length; i++) {
 
-        if(parseInt(data.attractions[i].distance) <= maxDistance) {
+        if (parseInt(data.attractions[i].distance) <= maxDistance) {
             list.push(data.attractions[i]);
         }
     }
@@ -299,8 +305,9 @@ function getWeather(callback) {
     });
     req.end();
 }
+
 function randomArrayElement(array) {
     var i = 0;
     i = Math.floor(Math.random() * array.length);
-    return(array[i]);
+    return (array[i]);
 }
